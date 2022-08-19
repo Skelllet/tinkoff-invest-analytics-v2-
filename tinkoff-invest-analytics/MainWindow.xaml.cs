08055fec132a7d78001a4c122c774ed23d1b14a4
Ruslan <regoyan@gmail.com>
2022-08-19 11:18:32 +0300

    Начало положено

---
+++ tinkoff-invest-analytics/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+
+namespace tinkoff_invest_analytics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tinkoff_Info f = new Tinkoff_Info();
+
+            f.GetClient();
+        }
+    }
+}
---
+++ tinkoff-invest-analytics/Tinkoff_Info.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Tinkoff.InvestApi;
+using Tinkoff.InvestApi.V1;
+
+namespace tinkoff_invest_analytics
+{
+    /// <summary>
+    /// Information from Tinkoff Invest
+    /// </summary>
+    internal class Tinkoff_Info
+    {
+        static readonly string TOKEN = Environment.GetEnvironmentVariable("TINKOFF_TOKEN");
+        static readonly string BROKER_ACCOUNT_ID = Environment.GetEnvironmentVariable("TINKOFF_BROKER_ACCOUNT");
+        
+        public void GetClient()
+        {
+            // Собираем ServiceCollection с клиентом
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddInvestApiClient((_, settings) =>
+            {
+                settings.AccessToken = TOKEN;
+            });
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var client = serviceProvider.GetRequiredService<InvestApiClient>();
+        }
+
+        public void GetAccounts()
+        {
+            
+        }
+    }
+}
